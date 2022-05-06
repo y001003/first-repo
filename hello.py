215dc9c356b77a6c101593ac52e3bcc75c4f70f2
y001003 <y001003@naver.com>
2022-05-06 13:23:26 +0900

    if case added 15 % == 0

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 for i in range(1,21):
-	if i % 3 == 0:
-		print(i,'year')
+	if i % 3 == 0 and i % 5 ==0:
+		print(i,'yeardream')
 	elif i % 5 == 0:
 		print(i,'dream')
+	elif i % 3 == 0 :
+		print(i,'year')
 	else:
 		print(i)
 
